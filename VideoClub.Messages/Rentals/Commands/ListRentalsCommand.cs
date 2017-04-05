ec35fe520f39018d421c688d2447d7a22dd9c967
Matias Hegoburu <matias.hegoburu@globallogic.com>
2017-04-05 10:52:38 -0300

    missing files

---
+++ VideoClub.Messages/Rentals/Commands/ListRentalsCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub.Messages.Rentals.Commands
+{
+    public interface IListRentalsCommand { }
+
+    public class ListRentalsCommand : IListRentalsCommand { }
+}
